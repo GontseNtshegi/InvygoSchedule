d5522c86cc02a0de8326884342fd822d11203bd7
Gontse Ntshegi <gontse.ntshegi@gmail.com>
2022-05-26 01:03:03 +0200

    Use webflux on database

--- src/main/java/com/invygo/digital/config/Constants.java
+++ src/main/java/com/invygo/digital/config/Constants.java
@@ -4,6 +4,7 @@ package com.invygo.digital.config;
  * Application constants.
  */
 public final class Constants {
+    public static final String LOGIN_REGEX = "^(?>[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*)|(?>[_.@A-Za-z0-9-]+)$";
 
     public static final String SYSTEM = "system";
 
---
+++ src/main/java/com/invygo/digital/repository/AuthorityRepository.java
@@ -0,0 +1,10 @@
+package com.invygo.digital.repository;
+
+
+import com.invygo.digital.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
